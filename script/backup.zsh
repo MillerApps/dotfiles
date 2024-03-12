03200e17dc2866d58bfdc94817876ca21d69975b
MillerApps <tyemiller4@gmail.com>
2024-03-12 14:56:53 -0400

    add backup script

---
+++ script/backup.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Navigate to directory
+# cd /Users/tylermiller/New-Machine/
+
+# Run the brew bundle dump
+brew bundle dump --force
+
+# Run Mackup
+mackup backup --force
+mackup uninstall --force
+
+# Git
+git add Brewfile
+git commit -m "Auto-update Brewfile"
+git push
