f4da27c52587781373c5ab541bbf0253098aafc6
MillerApps <tyemiller4@gmail.com>
2024-03-17 13:37:40 -0400

    update script for general weekly tasks

--- script/backup.zsh
+++ script/backup.zsh
@@ -3,6 +3,13 @@
 # Navigate to directory
 cd /Users/tylermiller/New-Machine/
 
+# Update brew formulas and casks
+/opt/homebrew/bin/brew update && /opt/homebrew/bin/brew upgrade
+/opt/homebrew/bin/brew upgrade --cask --greedy
+
+# Brew cleanup
+/opt/homebrew/bin/brew cleanup --prune=all
+
 # Run the brew bundle dump
 /opt/homebrew/bin/brew bundle dump --describe --force
 
