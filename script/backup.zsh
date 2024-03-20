5b7b1b82be8995ba83a05dd4009cb045ebd2d0e8
MillerApps <tyemiller4@gmail.com>
2024-03-20 00:11:37 -0400

    fix wallpaper copy

--- script/backup.zsh
+++ script/backup.zsh
@@ -23,6 +23,8 @@ last_change_timestamp=$(cat "$timestamp_file")
 # Get path of current desktop wallpaper using osascript
 wallpaper_path=$(osascript -e 'tell app "finder" to get posix path of (get desktop picture as alias)' 2>/dev/null)
 
+backup_directory="/Users/tylermiller/New-Machine/"
+
 if [ -z "$wallpaper_path" ]; then
     echo "No wallpaper change detected."
 else
@@ -32,13 +34,13 @@ else
 
     # Check if wallpaper has been changed recently
     if [ "$last_change_timestamp" != "$current_timestamp" ]; then
+        # Copy orginal file to repo
+        cp "$wallpaper_path" "$backup_directory"
+
         # Rename the wallpaper to Desktop.png
-        new_wallpaper_path=$(dirname "$wallpaper_path")/Desktop.png
-        mv "$wallpaper_path" "$new_wallpaper_path"
-
-        # Copy the renamed desktop wallpaper to your backup repository
-        cp "$new_wallpaper_path" "/Users/tylermiller/New-Machine/"
-
+        new_wallpaper_path=$backup_directory/Desktop.png
+        mv "$backup_directory/$(basname $wallpaper_path)" "$new_wallpaper_path"
+        
         # Update timestamp of last wallpaper change
         echo "$current_timestamp" > "$timestamp_file"
 
