179a4f6db479394a644f6c3dbea8e36a60d57595
MillerApps <tyemiller4@gmail.com>
2024-07-31 09:48:00 -0400

    update dock apps

--- script/dock.zsh
+++ script/dock.zsh
@@ -26,17 +26,20 @@ dockutil --remove all --no-restart
 dockutil --add  /Applications/Spotify.app --position 1 --no-restart
 dockutil --add  /Applications/Arc.app --position 2 --no-restart
 dockutil --add  /Applications/Telegram.app --position 3 --no-restart
-dockutil --add  /System/Applications/Messages.app --position 4 --no-restart
-dockutil --add  /Applications/Xcode.app --position 5 --no-restart
-dockutil --add  /Applications/Visual\ Studio\ Code.app --position 6 --no-restart
-dockutil --add  /Applications/Zed.app --position 7 --no-restart
-dockutil --add  /Applications/Warp.app --position 8 --no-restart
-dockutil --add  /Applications/Obsidian.app --position 9 --no-restart
-dockutil --add  /Applications/Linear.app --position 10 --no-restart
-dockutil --add  /Applications/Keka.app --position 11 --no-restart
+dockutil --add /Applications/Signal.app --position 4 --no-restart
+dockutil --add /Applications/Discord.app --position 5 --no-restart
+dockutil --add  /System/Applications/Messages.app --position 6 --no-restart
+dockutil --add  /Applications/Xcode.app --position 7 --no-restart
+dockutil --add  /Applications/Visual\ Studio\ Code.app --position 8 --no-restart
+dockutil --add  /Applications/Zed.app --position 9 --no-restart
+dockutil --add  /Applications/kitty.app --position 10 --no-restart
+dockutil --add  /Applications/Obsidian.app --position 11 --no-restart
+dockutil --add  /Applications/Linear.app --position 12 --no-restart
+dockutil --add  /Applications/Keka.app --position 13 --no-restart
 
 # Add folder/directory as stack
 dockutil --add '~/Downloads' --view grid --display folder --allhomes --no-restart
+dockutil --add '/Applications/' --view grid --dispaly stack --allhomes --no-restart
 
 # Restart dock to apply changes
 killall Dock
