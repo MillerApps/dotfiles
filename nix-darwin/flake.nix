190dc8a879e3141d0a3bac722251f7d11de164eb
MillerApps <tylermiller4.github@proton.me>
2024-12-31 02:50:02 -0500

    add mac settings

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -50,7 +50,34 @@
           fi
         '';
       };
-
+       
+      # Macos settings
+      security.pam.enableSudoTouchIdAuth = true;
+      system.defaults = {
+        dock.persistent-apps = [
+            "/Applications/Zen Browser.app"
+            "/Applications/Proton Mail.app"
+            "/Applications/Signal.app"
+            "/Applications/Ghostty.app"
+          ];
+          NSGlobalDomain.AppleInterfaceStyle = "Dark";
+          dock.minimize-to-application = true;
+          dock.persistent-others = [
+            # From what i saw on https://mynixos.com/nix-darwin/option/system.defaults.dock.persistent-others
+            # this should not be Necessary ~/Downloads/ should work but nope
+             "/Users/tylermiller/Downloads/"
+          ];
+          dock.show-recents = false;
+          dock.tilesize = 50;
+          trackpad.TrackpadRightClick = true;
+          NSGlobalDomain."com.apple.swipescrolldirection" = false;
+          finder.FXDefaultSearchScope = "SCcf";
+          finder.ShowExternalHardDrivesOnDesktop = true;
+          finder.ShowHardDrivesOnDesktop = true;
+          finder.ShowMountedServersOnDesktop = true;
+          finder.ShowPathbar = true;
+      };
+  
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
