abe2881ad041426706e44dbbecd75ca7675e94f9
MillerApps <tylermiller4.github@proton.me>
2024-12-31 22:23:13 -0500

    fix paths (pure now)

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -19,13 +19,13 @@
   home.stateVersion = "24.11";
 
   home.file = {
-    ".zshrc".source = ~/dotfiles/.zshrc;
-    ".config/ghostty".source = ~/dotfiles/ghostty;
-    ".p10k.zsh".source = ~/dotfiles/.p10k.zsh;
-    ".config/yazi".source = ~/dotfiles/yazi;
-    ".config/karabiner".source = ~/dotfiles/karabiner;
-    ".wezterm.lua".source = ~/dotfiles/.wezterm.lua;
-    ".oh-my-zsh".source = ~/dotfiles/.oh-my-zsh;
+    ".zshrc".source = ../.zshrc;
+    ".config/ghostty".source = ../ghostty;
+    ".p10k.zsh".source = ../.p10k.zsh;
+    ".config/yazi".source = ../yazi;
+    ".config/karabiner".source = ../karabiner;
+    ".wezterm.lua".source = ../.wezterm.lua;
+    # ".oh-my-zsh".source = ~/dotfiles/.oh-my-zsh;
   };
 
   # Let Home Manager install and manage itself.
