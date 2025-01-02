1808302faa1468c92f47461e592d93a72ebfb1e8
MillerApps <tylermiller4.github@proton.me>
2025-01-01 22:55:06 -0500

    remove activationscript as it does work as intended

--- nix-darwin/modules/macos.nix
+++ nix-darwin/modules/macos.nix
@@ -7,7 +7,7 @@
       "/Applications/Zen Browser.app"
       "/Applications/Proton Mail.app"
       "/Applications/Signal.app"
-      /* "/nix/store/rvlqdas14krhh86z1n9safky4rkfyksa-system-applications/Applications/Ghostty.app" */
+      "/Applications/Ghostty.app"
     ];
     NSGlobalDomain.AppleInterfaceStyle = "Dark";
     dock.minimize-to-application = true;
@@ -34,30 +34,4 @@
       };
     };
   };
-
-  system.activationScripts.postActivation.text = ''
-          # Get the Nix store path of your app
-          # we need to do it this way as to avoid
-          # an nasty symlink icon overlay
-          app_path="${pkgs.ghostty}/Applications/Ghostty.app"
-
-          # Add to dock using defaults
-          # this be added to the end of the dock
-          # even after open apps which are not persistent
-          defaults write com.apple.dock persistent-apps -array-add "<dict>
-          <key>tile-data</key>
-          <dict>
-          <key>file-data</key>
-          <dict>
-          <key>_CFURLString</key>
-          <string>$app_path</string>
-          <key>_CFURLStringType</key>
-          <integer>0</integer>
-          </dict>
-          </dict>
-          </dict>"
-
-          # Restart Dock to apply changes
-          killall Dock
-          '';
 }
