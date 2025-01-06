9a5d4439d3df024bf6719b82033e9f7f047e0e7c
MillerApps <tylermiller4.github@proton.me>
2025-01-06 16:23:59 -0500

    add ripgrep

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -14,81 +14,96 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nix-homebrew, neovim-config, ... }:
-    let
-      modules = import ./modules;
-      configuration = { pkgs, config, ... }: {
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-        environment.systemPackages =  with pkgs; [
-          neovim
-          git
-          lazygit
-          bat
-          btop
-          eza
-          gh
-          fzf
-          yazi
-          zoxide
-          thefuck
-          zsh-autosuggestions
-          zsh-syntax-highlighting
-          lua
-          go
-          serpl
-          nixd
-        ];
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+    home-manager,
+    nix-homebrew,
+    neovim-config,
+    ...
+  }: let
+    modules = import ./modules;
+    configuration = {
+      pkgs,
+      config,
+      ...
+    }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        neovim
+        git
+        lazygit
+        bat
+        btop
+        eza
+        gh
+        fzf
+        yazi
+        zoxide
+        thefuck
+        zsh-autosuggestions
+        zsh-syntax-highlighting
+        lua
+        go
+        serpl
+        nixd
+        alejandra
+        rip
+        ripgrep
+      ];
 
-        users.users.tylermiller.home = "/Users/tylermiller";
+      users.users.tylermiller.home = "/Users/tylermiller";
 
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
 
-        # Enable alternative shell support in nix-darwin.
-        # programs.fish.enable = true;
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
 
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
 
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 5;
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
 
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
 
-      };
-    in
-      {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#macbook
-      darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
-        modules = [
-          configuration
-          modules.homebrew
-          modules.macos
-          modules.zsh
-          # home-manager as a module
-          home-manager.darwinModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.tylermiller = import ./home.nix;
-            home-manager.extraSpecialArgs = { inherit neovim-config; };
-          }
-          nix-homebrew.darwinModules.nix-homebrew {
-            nix-homebrew = {
-              enable = true;
-              # Apple Silicon Only
-              enableRosetta = true;
-              # User owning the Homebrew prefix
-              user = "tylermiller";
-
-              autoMigrate = true;
-            };
-          }
-        ];
-      };
+      nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
     };
-}
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#macbook
+    darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        modules.homebrew
+        modules.macos
+        modules.zsh
+        # home-manager as a module
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.tylermiller = import ./home.nix;
+          home-manager.extraSpecialArgs = {inherit neovim-config;};
+        }
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            # Apple Silicon Only
+            enableRosetta = true;
+            # User owning the Homebrew prefix
+            user = "tylermiller";
 
+            autoMigrate = true;
+          };
+        }
+      ];
+    };
+  };
+}
