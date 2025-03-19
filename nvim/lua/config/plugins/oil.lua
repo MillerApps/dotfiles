16b0861fb5e49fe296c17fab6c2875a7d8011baf
MillerApps <tylermiller4.github@proton.me>
2025-03-18 20:02:16 -0400

    chore: add options to oil.nvim

--- nvim/lua/config/plugins/oil.lua
+++ nvim/lua/config/plugins/oil.lua
@@ -1,6 +1,9 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    delete_to_trash = true,
+    skip_confrim_fro_simple_edits = true,
+  },
   lazy = false,
   vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
 }
