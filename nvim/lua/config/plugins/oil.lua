ce2ee4a678d40057110f827e24a07a8b23d5803a
MillerApps <tylermiller4.github@proton.me>
2025-03-18 20:05:45 -0400

    fix: typo

--- nvim/lua/config/plugins/oil.lua
+++ nvim/lua/config/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
   'stevearc/oil.nvim',
   opts = {
     delete_to_trash = true,
-    skip_confrim_fro_simple_edits = true,
+    skip_confrim_from_simple_edits = true,
   },
   lazy = false,
   vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
