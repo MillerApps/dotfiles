f1aee03361e7c992cd2ea4d110c56a0c17b56f45
MillerApps <tylermiller4.github@proton.me>
2025-03-23 01:57:08 -0400

    fix: lix install

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -4,9 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 
     nix-darwin = {
