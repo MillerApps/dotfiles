9e050de3bf476eb65b9ce8c2b9b41a466414e22f
MillerApps <tylermiller4.github@proton.me>
2025-05-25 00:02:18 -0400

    fix (nix-darwin): remove user from postActivation

--- nix-darwin/modules/darwin/macos.nix
+++ nix-darwin/modules/darwin/macos.nix
@@ -89,7 +89,7 @@
     };
   };
 
-  system.activationScripts.postUserActivation.text = ''
+  system.activationScripts.postActivation.text = ''
     # Following line should allow us to avoid a logout/login cycle
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 
