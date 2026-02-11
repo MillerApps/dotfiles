4b9b950a6e7c9372d92553beefa4cea8123e343a
MillerApps <tylermiller4.github@proton.me>
2026-02-11 15:47:55 -0500

    fix: configure blink lazydev provider

--- nvim/lua/config/plugins/blink.lua
+++ nvim/lua/config/plugins/blink.lua
@@ -51,7 +51,17 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        per_filetype = {
+          lua = { inherit_defaults = true, 'lazydev' },
+        },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          },
+        },
       },
     },
     opts_extend = { 'sources.default' },
